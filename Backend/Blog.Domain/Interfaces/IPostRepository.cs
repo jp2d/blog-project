bbc9b42327115835ef81131826b393e06a3954b6
João Paulo Pimentel Dias <jp2d10@gmail.com>
2026-02-13 15:12:52 -0300

    Criação das interfaces de repositório

---
+++ Backend/Blog.Domain/Interfaces/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using Blog.Domain.Entity;
+
+namespace Blog.Domain.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetAllAsync();
+        Task<Post?> GetByIdAsync(int id);
+        Task AddAsync(Post post);
+        Task UpdateAsync(Post post);
+        Task DeleteAsync(int id);
+    }
+}
