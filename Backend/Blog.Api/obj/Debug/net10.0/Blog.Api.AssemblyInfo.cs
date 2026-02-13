e201489b75e35b6d5b31bd34458766733e25b3ac
João Paulo Pimentel Dias <jp2d10@gmail.com>
2026-02-13 12:17:12 -0300

    Configuração do Swagger UI

--- Backend/Blog.Api/obj/Debug/net10.0/Blog.Api.AssemblyInfo.cs
+++ Backend/Blog.Api/obj/Debug/net10.0/Blog.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3c408c7dd85c3885c1107c969d794d663927dbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77151532b93458e060684fba130a055eabfecb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
