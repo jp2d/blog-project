0284ff2915a78c8576c230f0be83ce5f15ec42ee
João Paulo Pimentel Dias <jp2d10@gmail.com>
2026-02-14 21:14:30 -0300

    Criação e configuração do JWT

---
+++ Backend/Blog.Applications/Services/JwtTokenService.cs
@@ -0,0 +1,36 @@
+﻿using Blog.Applications.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace Blog.Applications.Services
+{
+    public class JwtTokenService : IJwtTokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenService(IConfiguration configuration) => _configuration = configuration;
+        public string GenerateToken(string userId, string role)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"] ?? throw new InvalidOperationException("Jwt:Key não configurado")));
+            var credencial = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: credencial);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
