1c2a6eb0754d8b0da02133fa6ee7f4d27df54ba0
João Paulo Pimentel Dias <jp2d10@gmail.com>
2026-02-17 17:07:46 -0300

    Ajustes nas Entidades

--- Backend/Blog.Domain/Entity/User.cs
+++ Backend/Blog.Domain/Entity/User.cs
@@ -7,7 +7,7 @@ namespace Blog.Domain.Entity
         public int Id { get; set; }
         public required string Name { get; set; }
         public required string Email { get; set; }
-        public required string PasswordHash { get; set; }
+        public string PasswordHash { get; set; }
         public Roles Role { get; set; } = Roles.User;
         public ICollection<Post>? Posts { get; set; }
     }
