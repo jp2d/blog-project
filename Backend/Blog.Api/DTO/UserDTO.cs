566f752079e71c8b0d7f8d09de5f641593264df9
João Paulo Pimentel Dias <jp2d10@gmail.com>
2026-02-17 17:01:41 -0300

    Criação dos DTOs

---
+++ Backend/Blog.Api/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Domain.Enum;
+using Microsoft.AspNetCore.Identity;
+
+namespace Blog.Api.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public Roles Role { get; set; }
+        public IEnumerable<PostDto>? Posts { get; set; }
+    }
+}
